a4b368af8b5f073bcfe6de58d05c77a794fddf73
123 <a123@123nomacbook-pro.local>
2019-02-05 22:15:21 +0900

    Initial Commit

---
+++ MyFoodExpense/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFoodExpense
+//
+//  Created by 123 on 2019/02/05.
+//  Copyright © 2019 123. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
