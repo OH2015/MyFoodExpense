fcd67e52f959fa76e156f267f8a8a9bff30aec4b
123 <a123@123nomacbook-pro.local>
2019-02-13 18:50:37 +0900

    キーボードが隠れない仕様を追加

--- MyFoodExpense/popUpViewController.swift
+++ MyFoodExpense/popUpViewController.swift
@@ -8,12 +8,21 @@
 
 import UIKit
 
-class popUpViewController: UIViewController {
+class popUpViewController: UIViewController ,UITextFieldDelegate{
 
     @IBOutlet weak var titleText: UITextField!
+    @IBOutlet weak var storeButton: UIButton!
+
+    let SCREEN_SIZE = UIScreen.main.bounds.size
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        titleText.delegate = self
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        //ここでUIKeyboardWillShowという名前の通知のイベントをオブザーバー登録をしている
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        //ここでUIKeyboardWillHideという名前の通知のイベントをオブザーバー登録をしている
 
     }
 
@@ -25,6 +34,41 @@ class popUpViewController: UIViewController {
                 dismiss(animated: true, completion: nil)
             }
         }
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        titleText.endEditing(true)
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        titleText.endEditing(true)
+        return true
+    }
+
+    @objc func keyboardWillShow(_ notification: NSNotification){
+        let rect = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue
+        let margin = SCREEN_SIZE.height - (storeButton.frame.origin.y + 250 + storeButton.frame.size.height + (rect?.size.height)!)
+//        let duration: TimeInterval? = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double
+        if  margin < 0{
+            print("ok")
+            UIView.animate(withDuration: 1.0, animations: { () in
+                let transform = CGAffineTransform(translationX: 0, y: margin)
+                self.view.transform = transform
+            })
+        }
+    }
+
+    @objc func keyboardWillHide(_ notification: NSNotification){
+//        let duration: TimeInterval? = notification.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? Double
+        UIView.animate(withDuration: 1.0, animations: { () in
+
+            self.view.transform = CGAffineTransform.identity
+        })
+    }
+
+    func removeObserver() {
+        let notification = NotificationCenter.default
+        notification.removeObserver(self)
     }
     
     @IBAction func buttonTapped(_ sender: Any) {
