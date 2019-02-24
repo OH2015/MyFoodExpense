ed86bf617d9bb3fe876766eee486b02cd6b245ec
123 <a123@123noMacBook-Pro.local>
2019-02-24 14:04:50 +0900

    スタックビューのレイアウト

---
+++ MyFoodExpense/Box.swift
@@ -0,0 +1,19 @@
+//
+//  Box.swift
+//  MyFoodExpense
+//
+//  Created by 123 on 2019/02/24.
+//  Copyright © 2019 123. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Box{
+    var ingredient = ""
+    var tax = "税抜き"
+    var title = ""
+    var cost = 0
+    var person = 0
+    var date:Date?
+}
--- MyFoodExpense/ViewController.swift
+++ MyFoodExpense/ViewController.swift
@@ -6,10 +6,14 @@ import UIKit
 import Charts
 
 class viewController:UIViewController{
+    let box1 = Box()
+    let box2 = Box()
+    let box3 = Box()
+    let box4 = Box()
+    let box5 = Box()
+    let box6 = Box()
 
-
-    override func ViewDidLoad(){
-        super.viewDidLoad()
-
+    override func viewDidLoad() {
+        <#code#>
     }
 }
