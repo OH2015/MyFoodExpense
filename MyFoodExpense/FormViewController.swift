805eb9b34783a1cdb748ba293d8382a9a1168444
123 <a123@123noMacBook-Pro.local>
2019-02-26 16:51:23 +0900

    入力画面をテーブルビューを使って実装する

---
+++ MyFoodExpense/FormViewController.swift
@@ -0,0 +1,32 @@
+//
+//  FormViewController.swift
+//  MyFoodExpense
+//
+//  Created by 123 on 2019/02/26.
+//  Copyright © 2019 123. All rights reserved.
+//
+
+import UIKit
+
+class FormViewController: UIViewController ,UITableViewDelegate,UITableViewDataSource{
+
+
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+
+
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
+    }
+    
+}
---
+++ MyFoodExpense/InputTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  InputTableViewCell.swift
+//  
+//
+//  Created by 123 on 2019/02/26.
+//
+
+import UIKit
+
+class InputTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        
+    }
+
+}
