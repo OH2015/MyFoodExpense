9ec1c3ab4c101f10b61d46ba2902896bb17306ea
123 <a123@123noMacBook-Pro.local>
2019-02-26 20:23:56 +0900

    ピッカーでコストを入力できるようになった

--- MyFoodExpense/FormViewController.swift
+++ MyFoodExpense/FormViewController.swift
@@ -8,25 +8,83 @@
 
 import UIKit
 
-class FormViewController: UIViewController ,UITableViewDelegate,UITableViewDataSource{
+class FormViewController: UIViewController ,UITableViewDelegate,UITableViewDataSource,UIPickerViewDelegate,UIPickerViewDataSource{
 
 
+    //ピッカービュー
+    private var pickerView:UIPickerView!
+    private let pickerViewHeight:CGFloat = 160
+
+    //pickerViewの上にのせるtoolbar
+    private var pickerToolbar:UIToolbar!
+    private let toolbarHeight:CGFloat = 40.0
 
     @IBOutlet weak var tableView: UITableView!
+//---------------------------------viewDidLoad----------------------------------------------
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
 
+        let width = self.view.frame.width
+        let height = self.view.frame.height
+
+        //pickerView
+        pickerView = UIPickerView(frame:CGRect(x:0,y:height + toolbarHeight,
+                                               width:width,height:pickerViewHeight))
+        pickerView.dataSource = self
+        pickerView.delegate = self
+        pickerView.backgroundColor = UIColor.gray
+        self.view.addSubview(pickerView)
+
 
     }
+//----------------------------------tableView----------------------------------------------
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 5
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+
+            
         return cell
     }
-    
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+    }
+
+//------------------------------------pickerView-------------------------------------------
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 4
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 10
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(row)
+    }
+
+//------------------------------------------------------------------------------------------
+
+    @IBAction func wrote(_ sender: UITextField) {
+    }
+
+    @IBAction func costSelect(_ sender: PickerTextField) {
+        sender.setup()
+    }
+//-------------------------------------------------------------------------------------------
+    func doneTapped(){
+        UIView.animate(withDuration: 0.2){
+            self.pickerToolbar.frame = CGRect(x:0,y:self.view.frame.height,
+                                              width:self.view.frame.width,height:self.toolbarHeight)
+            self.pickerView.frame = CGRect(x:0,y:self.view.frame.height + self.toolbarHeight,
+                                           width:self.view.frame.width,height:self.pickerViewHeight)
+            self.tableView.contentOffset.y = 0
+        }
+    }
 }
+
---
+++ MyFoodExpense/PickerTextField.swift
@@ -0,0 +1,82 @@
+//
+//  PickerTextField.swift
+//  MyFoodExpense
+//
+//  Created by 123 on 2019/02/26.
+//  Copyright © 2019 123. All rights reserved.
+//
+
+import UIKit
+
+class PickerTextField: UITextField ,UIPickerViewDataSource,UIPickerViewDelegate{
+
+    var thou = 0
+    var hund = 0
+    var ten = 0
+    var one = 0
+    var number = 0
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 4
+    }
+
+    init() {
+        super.init(frame: CGRect.zero)
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+
+    func setup() {
+
+        let picker = UIPickerView()
+        picker.delegate = self
+        picker.dataSource = self
+        picker.showsSelectionIndicator = true
+
+        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 0, height: 35))
+        let doneItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        let cancelItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancel))
+        toolbar.setItems([cancelItem, doneItem], animated: true)
+
+        self.inputView = picker
+        self.inputAccessoryView = toolbar
+    }
+
+
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 10
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(row)
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch component {
+        case 0:thou = row
+        case 1:hund = row
+        case 2:ten = row
+        case 3:one = row
+        default:break}
+        number = thou*1000 + hund*100 + ten*10 + one
+        self.text = String(number)
+    }
+
+    @objc func cancel() {
+        self.text = ""
+        self.endEditing(true)
+    }
+
+    @objc func done() {
+        self.endEditing(true)
+    }
+
+}
